3d97110050534dfa6f23541d0df6dbd246ebcf4b
Jeffrey Chan <jeffreychan1303@gmail.com>
2023-11-21 09:41:59 -0500

    feature: add sotring functionality

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,17 @@ import type { PetProps } from './types';
 
 type PetsProps = Array<PetProps>;
 
+enum SortByValue {
+  None,
+  Alphabetically,
+  ReverseAlphabetically,
+}
+
 function App() {
   const [allPets, setAllPets] = useState<PetsProps>([]);
   const [selectedPets, setSelectedPets] = useState<PetsProps>([]);
+  const [search, setSearch] = useState('');
+  const [sortByValue, setSortByValue] = useState<SortByValue>(SortByValue.None);
 
   useEffect(() => {
     getPets();
@@ -18,7 +26,6 @@ function App() {
       const response = await fetch('https://eulerity-hackathon.appspot.com/pets');
       const data = await response.json();
       setAllPets(data);
-      console.log(data);
     } catch (e) {
       console.log(e);
     }
@@ -30,7 +37,6 @@ function App() {
     } else {
       setSelectedPets([...selectedPets, pet]);
     }
-    console.log(selectedPets);
   }
 
   const downloadSelectedPets = () => {
@@ -63,22 +69,43 @@ function App() {
       <h1>Pet Image Downloader</h1>
       <h4>Choose the images that you wish to download!</h4>
 
-      <div
-        className="actions-bar"
-        style={{ display: 'flex', justifyContent: 'center', gap: '2rem', padding: '2rem' }}
-      >
+      <div className="actions-bar">
         <button onClick={() => setSelectedPets(allPets)}>Select All</button>
         <button onClick={() => setSelectedPets([])}>Clear Selection</button>
         <button onClick={() => downloadSelectedPets()}>Download Selection</button>
+        <select value={sortByValue} onChange={(e) => setSortByValue(parseInt(e.target.value))}>
+          <option value={SortByValue.None}>No Sorting</option>
+          <option value={SortByValue.Alphabetically}>Sort By A-Z</option>
+          <option value={SortByValue.ReverseAlphabetically}>Sort By Z-A</option>
+        </select>
+        <div className="search-wrapper">
+          <label htmlFor="search">Search: </label>
+          <input name="search" type="text" onChange={(e) => setSearch(e.target.value.trim())} />
+        </div>
       </div>
       <div className="card-selection">
-        {allPets.map((pet) => (
-          <PetCard
-            pet={pet}
-            onClick={() => selectDeselectPet(pet)}
-            isSelected={selectedPets.indexOf(pet) > -1}
-          />
-        ))}
+        {/* filter, sort, and map pets depending on app state */}
+        {allPets
+          .filter(
+            (curPet) =>
+              curPet.title.toLowerCase().includes(search.toLowerCase()) ||
+              curPet.description.toLowerCase().includes(search.toLowerCase())
+          )
+          .sort((a, b) => {
+            if (sortByValue === SortByValue.Alphabetically) {
+              return a.title.localeCompare(b.title);
+            } else if (sortByValue === SortByValue.ReverseAlphabetically) {
+              return b.title.localeCompare(a.title);
+            }
+            return 0;
+          })
+          .map((pet) => (
+            <PetCard
+              pet={pet}
+              onClick={() => selectDeselectPet(pet)}
+              isSelected={selectedPets.indexOf(pet) > -1}
+            />
+          ))}
       </div>
     </>
   );
