ea2ac1c88ee2aaf691eeadcb11429742186c9baf
Jeffrey Chan <jeffreychan1303@gmail.com>
2023-11-23 17:06:07 -0500

    update about me page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,3 +1,9 @@
+import styled from 'styled-components';
+
+const StyledListItem = styled.li`
+  list-style-type: none;
+`;
+
 const About = () => {
   return (
     <div>
@@ -6,6 +12,34 @@ const About = () => {
       <h4>
         I'm a University student studying computer science expected to graduate December 2024.
       </h4>
+
+      <hr />
+
+      <h2>Check me out through these other websites!</h2>
+      <ul>
+        <StyledListItem>
+          Linkedin:{' '}
+          <a
+            href="https://www.linkedin.com/in/jeffrey-chan-1303/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            https://www.linkedin.com/in/jeffrey-chan-1303/
+          </a>
+        </StyledListItem>
+        <StyledListItem>
+          Github:{' '}
+          <a href="https://github.com/JeffreyChan1303" target="_blank" rel="noopener noreferrer">
+            https://github.com/JeffreyChan1303
+          </a>
+        </StyledListItem>
+        <StyledListItem>
+          Portfolio:{' '}
+          <a href="https://www.jeffreychan.dev/" target="_blank" rel="noopener noreferrer">
+            https://www.jeffreychan.dev/
+          </a>
+        </StyledListItem>
+      </ul>
     </div>
   );
 };
